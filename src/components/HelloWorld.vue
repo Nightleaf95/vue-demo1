c1b84aa43b404b4405b65801de0e9063a21d68fe
Apple <batman@YeKaiQis-MacBook-Pro.local>
2019-12-31 17:26:47 +0800

    V-1.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>i wanna fly</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.hello{
+  h1{
+    color: red;
+  }
+}
+</style>
